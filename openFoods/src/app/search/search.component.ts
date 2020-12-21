f1a465bdd6319bf33845ec80bce0af3bdf8927cb
fabioventu <venturini.fabio@iisgalvanimi.edu.it>
2020-12-21 11:02:43 +0000

    punto5

--- openFoods/src/app/search/search.component.ts
+++ openFoods/src/app/search/search.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { FoodService } from '../food.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent {
+  query: string;
+  title = 'food';
+  obsTrack: Observable<Object>;
+  results: any;
 
-  constructor() { }
 
-  ngOnInit(): void {
+
+  constructor(private food:FoodService) {
+
+   }
+
+
+   submit(query: HTMLInputElement): void {
+
+    if (!query.value) {
+      return;
+    }
+    this.query = query.value;
+    this.obsTrack = this.food.searchFood(this.query);
+    this.obsTrack.subscribe((data) => { this.results = data; console.log(this.results) });
   }
 
 }
