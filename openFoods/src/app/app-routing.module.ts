2cfadd10885233055b2324fbc3fde15bf674c9bb
fabioventu <venturini.fabio@iisgalvanimi.edu.it>
2020-12-21 10:29:04 +0000

    punto2

--- openFoods/src/app/app-routing.module.ts
+++ openFoods/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SearchComponent } from './search/search.component';
+import { ItemComponent } from './item/item.component';
 
-const routes: Routes = [];
 
+const routes: Routes = [
+  { path: 'item/:id', component: ItemComponent },
+  { path: 'search', component: SearchComponent }
+
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
